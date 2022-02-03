4e98a390a15566f01221aec18c38ad332bb83dbd
Anderson Roque <andersonbarrinha18@gmail.com>
2022-02-03 11:26:11 -0300

    Finazalidado

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,30 @@
-export function SideBar() {
+import React from 'react';
+import { Button } from './Button';
+import '../styles/sidebar.scss';
+import { GenreResponseProps } from '../services/interface';
+
+interface SideBarProps {
+  genres:GenreResponseProps[];
+  selectedGenredId:number;
+  handleClick(id: number): void;
+}
+
+export function SideBar({genres,selectedGenredId,handleClick}:SideBarProps) {
   // Complete aqui
+  return (<nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => handleClick(genre.id)}
+              selected={selectedGenredId === genre.id}
+            />
+          ))}
+        </div>
+
+      </nav>)
 }
